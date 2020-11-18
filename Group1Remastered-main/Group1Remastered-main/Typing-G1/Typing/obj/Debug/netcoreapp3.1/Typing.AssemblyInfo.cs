2d777c43a4fe4b0699de76bf0e33eea61a51aabf
Ian Brinkerhoff <ianbrinkerhoff@mail.weber.edu>
2020-11-18 12:57:41 -0700

    fixed multiplayer start game issue

--- Group1Remastered-main/Group1Remastered-main/Typing-G1/Typing/obj/Debug/netcoreapp3.1/Typing.AssemblyInfo.cs
+++ Group1Remastered-main/Group1Remastered-main/Typing-G1/Typing/obj/Debug/netcoreapp3.1/Typing.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("00852ee3-8198-4090-951e-7ad883e51740")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Typing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
---
+++ Group1Remastered-main/Group1Remastered-main/Typing-G1/Typing/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/chat.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/messageHub").build();
+var msg;
+
+connection.on("ReceiveMessage", function (username, input) {
+    msg = input;
+});
+
+connection.start().then(function () {
+}).catch(function (err) {
+    return console.error(err.string());
+});
+
+$('#tb1').keyup(function () {
+    var username = $('#currentUser').val();
+    var input = $('#tb1').val().toUpperCase();
+    connection.invoke("SendInput", username, input).catch(function (err) {
+        return console.error(err.toString());
+    });
+});
